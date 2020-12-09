8c53f2947f3aa16a47a5b35097bc8abbd814096e
Justin Kumpe <jakumpe@kumpes.com>
2020-12-08 18:29:26 -0600

    Initial Commit

---
+++ ContentRestrictionsKit/ContentRestrictionsKit.h
@@ -0,0 +1,18 @@
+//
+//  ContentRestrictionsKit.h
+//  ContentRestrictionsKit
+//
+//  Created by Justin Kumpe on 12/8/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ContentRestrictionsKit.
+FOUNDATION_EXPORT double ContentRestrictionsKitVersionNumber;
+
+//! Project version string for ContentRestrictionsKit.
+FOUNDATION_EXPORT const unsigned char ContentRestrictionsKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ContentRestrictionsKit/PublicHeader.h>
+
+
